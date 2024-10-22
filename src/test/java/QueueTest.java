4ced77cea86ecdeeaf522f9134a72355fd531ed4
jenna.nicodemus <jenna.nicodemus@cs-dolphin.centre.edu>
2024-10-22 14:35:18 -0400

    tests

---
+++ src/test/java/QueueTest.java
@@ -0,0 +1,43 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class QueueTest
+{
+
+	Queue<String> queueA;
+	
+	@BeforeEach
+	void setUp() throws Exception
+	{
+		queueA = new Queue<String>();
+	}
+
+	@Test
+	void testEmpty()
+	{
+		assertEquals(true, queueA.isEmpty());
+	}
+	
+	@Test
+	void testThrowException()
+	{
+		assertThrows(QueueUnderFlowException.class, () -> {
+			queueA.pop();
+		});
+	}
+	
+	@Test
+	void testPushPop() 
+	{
+		queueA.push("Hello");
+		queueA.push("Hi");
+		queueA.push("Greetings");
+		
+		assertEquals("Hello", queueA.pop());
+		assertEquals("Hi", queueA.pop());
+		assertEquals("Greetings", queueA.pop());
+	}
+
+}
